14ede08754fb379d03e2aa1dd9ba39281f698a15
femsworld <femsworld@gmail.com>
2023-07-06 22:57:05 +0300

    Created more tables

--- querries/createDatabase.sql
+++ querries/createDatabase.sql
@@ -1,3 +1,4 @@
+-- Creating employees table
 -- Table: application.employees
 
 -- DROP TABLE IF EXISTS application.employees;
@@ -21,3 +22,91 @@ ALTER TABLE IF EXISTS application.employees
     OWNER to "Femi";
 
 
+-- Creating hour_tracking table
+-- Table: application.hour_tracking
+
+-- DROP TABLE IF EXISTS application.hour_tracking;
+
+CREATE TABLE IF NOT EXISTS application.hour_tracking
+(
+    employee_id integer NOT NULL,
+    project_id integer,
+    total_hours integer NOT NULL,
+    CONSTRAINT employee_id_fk FOREIGN KEY (employee_id)
+        REFERENCES application.employees (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT project_id_fk FOREIGN KEY (project_id)
+        REFERENCES application.projects (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS application.hour_tracking
+    OWNER to "Femi";
+
+
+-- Creating projects table
+-- Table: application.projects
+
+-- DROP TABLE IF EXISTS application.projects;
+
+CREATE TABLE IF NOT EXISTS application.projects
+(
+    id integer NOT NULL DEFAULT nextval('application.projects_id_seq'::regclass),
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    client character varying COLLATE pg_catalog."default" NOT NULL,
+    start_date date NOT NULL,
+    deadline date NOT NULL,
+    CONSTRAINT projects_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS application.projects
+    OWNER to "Femi";
+
+
+
+-- Creating team_project table
+-- Table: application.team_project
+
+-- DROP TABLE IF EXISTS application.team_project;
+
+CREATE TABLE IF NOT EXISTS application.team_project
+(
+    team_id integer NOT NULL,
+    project_id integer NOT NULL,
+    CONSTRAINT project_id_fk FOREIGN KEY (project_id)
+        REFERENCES application.projects (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS application.team_project
+    OWNER to "Femi";
+
+
+-- Creating teams table
+-- Table: application.teams
+
+-- DROP TABLE IF EXISTS application.teams;
+
+CREATE TABLE IF NOT EXISTS application.teams
+(
+    id integer NOT NULL DEFAULT nextval('application.teams_id_seq'::regclass),
+    team_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    location character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT teams_pkey PRIMARY KEY (id),
+    CONSTRAINT team_name_unique UNIQUE (team_name)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS application.teams
+    OWNER to "Femi";
