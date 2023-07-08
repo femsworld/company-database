571a9de3251704dde428dd1ddd6332e741d2d3f6
femsworld <femsworld@gmail.com>
2023-07-08 22:17:25 +0300

    Added the create_project_with_teams function

--- querries/functions.sql
+++ querries/functions.sql
@@ -41,3 +41,32 @@ $BODY$;
 
 ALTER FUNCTION application.track_working_hours(integer, integer, numeric)
     OWNER TO postgres;
+
+
+-- Create a function `create_project_with_teams` to create a project 
+-- and assign teams to that project simultaneously. Test this function
+
+CREATE FUNCTION application.create_project_with_teams(IN p_project_name character varying, IN p_client character varying, IN p_start_date date, IN p_deadline date, IN p_team_ids integer[])
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    
+AS $BODY$
+declare
+    project_id integer;
+	team_id integer;
+begin
+    insert into application.projects (name, client, start_date, deadline)
+    values (p_project_name, p_client, p_start_date, p_deadline)
+    returning id into project_id;
+
+    foreach team_id in array p_team_ids loop
+        insert into application.team_project (team_id, project_id)
+        values (team_id, project_id);
+    end loop;
+
+    raise notice 'Project "%", ID % created successfully with teams: %', p_project_name, project_id, p_team_ids;
+end;
+$BODY$;
+
+ALTER FUNCTION application.create_project_with_teams(character varying, character varying, date, date, integer[])
+    OWNER TO postgres;
